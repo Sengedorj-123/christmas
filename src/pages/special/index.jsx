ac0aa2998aaef2b2378bb7ffa180fd7c4ca9b28b
Sengedorj <sengedorjj@gmail.com>
2025-12-24 06:20:49 +0800

    done

--- src/pages/special/index.jsx
+++ src/pages/special/index.jsx
@@ -3,6 +3,7 @@ import Head from "next/head";
  import { Header } from "@/components/Header";
 import { Snowfall } from "@/components/Snow";
 import { ChristmasBox } from "@/components/Box";
+import { Music } from "@/components/Music";
 export default function page() {
   return (
     <> 
@@ -17,6 +18,7 @@ export default function page() {
         <Snowfall />
         <Header />
         <ChristmasBox />
+        <Music />
       </div>
     </>
   );
